6567f37ac73dcfece92b2df6ac4ff01286176a44
Dan Edens <Danedens31@gmail.com>
2024-01-12 08:57:17 -0500

    adds daily standup launcher and regressionStartUp script. also adds additional screen layouts

--- init.lua
+++ init.lua
@@ -2,6 +2,8 @@
 
 dofile(hs.configdir .. "/loadConfig.lua")
 dofile(hs.configdir .. "/ExtendedClipboard.lua")
+
+-- dofile(hs.configdir .. "/workspace.lua")
 -- dofile(hs.configdir .. "/test_balena_handler.lua")
 dofile(hs.configdir .. "/hotkeys.lua")
 -- dofile(hs.configdir .. "/temp.lua")
@@ -21,7 +23,43 @@ end
 
 myWatcher = hs.pathwatcher.new(os.getenv("HOME") .. "/.hammerspoon/", reloadConfig):start()
 
-
 hs.alert.show("Config loaded")
+
+
+usbWatcher = nil
+function usbDeviceCallback(data)
+    print(data["productName"])
+    if (data["productName"] == "USB Keyboard") then
+        if (data["eventType"] == "added") then
+            hs.alert.show("USB Keyboard plugged in")
+        elseif (data["eventType"] == "removed") then
+            hs.alert.show("USB Keyboard unplugged")
+        end
+    end
+end
+usbWatcher = hs.usb.watcher.new(usbDeviceCallback)
+usbWatcher:start()
+
+-- everyday at 10:45 am
+hs.timer.doAt("10:45", "1d", function() 
+    hs.alert.show("Time to stand up!")
+    -- open chrome to a specific page
+    hs.execute("open -a 'Google Chrome' https://meet.google.com/xjk-uzpk-oit?authuser=1")
+end)
+
+-- ctrl + cmd + alt + shift + ` to switch to vs code
+hs.hotkey.bind({"ctrl", "cmd", "alt"}, "`", function()
+    hs.application.launchOrFocus("Visual Studio Code")
+end)
+
+
+
+--everyday at 10:45 am
+hs.timer.doAt("10:44", "1d", function() 
+    hs.alert.show("Time to stand up!")
+    -- open chrome to a specific page
+    hs.execute("open -a 'Google Chrome' https://meet.google.com/xjk-uzpk-oit?authuser=1")
+end)
+
 
 
---
+++ workspace.lua
@@ -0,0 +1,12 @@
+-- Workspace config
+--
+--
+--
+function regressionStartup()
+    hs.execute("pycharm ~/lab/regressiontestkit")
+    hs.execute("code ~/lab/regressiontestkit")
+      
+end
+
+
+
